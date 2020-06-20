d1f6029829ed2c98227fe47d9f87c622c350a45e
24kawerka <qwewerka@gmail.com>
2020-06-20 18:16:51 +0300

    Updates

---
+++ precache-manifest.294f1192aa9aff653ec3f04fa8d54966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d935d4aa790a0d22340d4e0ac20a288",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "d96a291c5f51b5736f7f",
+    "url": "/calendar/static/css/main.9724201a.chunk.css"
+  },
+  {
+    "revision": "e5bf2a4004e894291fda",
+    "url": "/calendar/static/js/2.708cf6c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar/static/js/2.708cf6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96a291c5f51b5736f7f",
+    "url": "/calendar/static/js/main.837c1055.chunk.js"
+  },
+  {
+    "revision": "5e1efa41e70e9f825195",
+    "url": "/calendar/static/js/runtime-main.05ca6ea8.js"
+  }
+]);
